d193edf2f63aac24c5c29e2f75fa9202d419d7fb
beni2412 <71620915+beni2412@users.noreply.github.com>
2020-09-21 09:55:02 +0530

    second push

--- src/main/java/com/javapractice/App.java
+++ src/main/java/com/javapractice/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-	LOG.info("Hi");
+	LOG.info("Holaa");
         LOG.info("Hello World");
     }
 }

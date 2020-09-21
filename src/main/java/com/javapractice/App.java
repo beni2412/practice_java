2be4b5bd3b0fbd71479d0018577339cb15894540
beni2412 <hardaman97@gmail.com>
2020-09-21 11:36:38 +0530

    second push

--- src/main/java/com/javapractice/App.java
+++ src/main/java/com/javapractice/App.java
@@ -12,7 +12,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-	LOG.info("Hi");
+	LOG.info("byeee");
+	LOG.info("Auf wiedersehen");
         LOG.info("Hello World");
     }
 }
